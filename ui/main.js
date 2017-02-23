38f22a9852c82049a02c19b17b301edcb348f4bd
thillairavi <thillairavi@gmail.com>
2017-02-23 02:13:32 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
+//change text to  new-text
 var element = document.getElementById("main-text");
 element.innerHTML="new value";
+
+//move the img to right
+var img = document.getElementById("madi");
+ img.onclick = function(){
+     img.style.marginLeft="100px";
+    
+};
