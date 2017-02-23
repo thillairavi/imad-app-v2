80343f4e7c2baa1ac684153288f7abdff797f701
thillairavi <thillairavi@gmail.com>
2017-02-23 02:35:37 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,13 @@ element.innerHTML="new value";
 
 //move the img to right
 var img = document.getElementById("madi");
-var marginLeft= 0;
+var marginLeft = 0;
 function moveRight(){
-   marginLeft= marginLeft + 10;
-   img.style.marginLeft= marginLeft +"px";
+   marginLeft = marginLeft + 10;
+   img.style.marginLeft = marginLeft + "px";
     
 }
  img.onclick = function(){
-     var interval=setInerval(moveRight,100);
+     var interval=setInerval(moveRight, 100);
     
 };
