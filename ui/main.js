49ecb66d6ec5b99a8193d024b8ede29e6e2febc3
thillairavi <thillairavi@gmail.com>
2017-02-25 10:06:15 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,3 +42,38 @@ button.onclick = function() {
     request.open("GET", "http://thillairavi.imad.hasura-app.io/counter", true);
     request.send(null);
 };
+
+
+
+//print the name when press submit
+var submit = document.getElementById("submit-btn");
+
+submit.onclick = function() {
+    //Make a request 
+    var request= new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take action
+            if(request.status === 200){
+                var names= request.responseText;
+                names= JSON.parse(names);
+                var list= '';
+                for(var i=0; i<= names.length; i++)
+                 list+= '<li>' + names[i] + '</li>';
+                 var ul = document.getElementById("namelist");
+                 ul.innerHTML= list;
+                
+            }
+          
+        }
+          //else not yet
+    };
+    var nameInput = document.getElementById("name");
+    var name= nameinput.value;
+    // make a request
+    request.open("GET", "http://thillairavi.imad.hasura-app.io/submit-name?name="+ name, true);
+    request.send(null);
+};
+
