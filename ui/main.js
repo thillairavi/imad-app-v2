58138726ff8f618ed90fcdd0176444aeafc42cf5
thillairavi <thillairavi@gmail.com>
2017-03-01 08:56:12 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,7 +62,7 @@ submit.onclick = function(){
                 var list= '';
                 for(var i=0; i< names.length; i++)
                  list+= '<li>' + names[i] + '</li>';
-                 var ul = document.getElementById("namelist");
+                // var ul = document.getElementById("namelist");
                  ul.innerHTML= list;
                 
             }
