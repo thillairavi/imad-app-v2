58c11981628fe9a657acad7be29c0bcf496939c9
thillairavi <thillairavi@gmail.com>
2017-03-03 06:48:29 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/submit-name', function (req, res) {
 
 res.status(40).send('article query');
 app.get("articles/:articleName", function(req,res) {
-    res.status(40).send('article query');
+    res.status(40).send('article query1');
      pool.query("SELECT * FROM article_w10 WHERE title = '" + req.params.articleName +"'", function(err,result){
         if(err){
          res.status(500).send(err,toString());
