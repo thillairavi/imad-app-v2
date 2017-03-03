80ba2e158e93e2ed9d3f7374a8dbdf378edd965c
thillairavi <thillairavi@gmail.com>
2017-03-03 08:16:27 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
