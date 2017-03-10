5c560ac36df952255f366da7a9c87b026f3943fa
thillairavi <thillairavi@gmail.com>
2017-03-10 09:44:04 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/test-db', function(req, res) {
 
 function hash(input, salt) {
     
-     var hashed= crypto.pbkdf2(input, salt, 10000, 512, 'sha512');
+     var hashed= crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
      return(hashed.toString('hex'));
 }
 
