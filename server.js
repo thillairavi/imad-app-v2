b8d952e126f7b48a58f8f51ea8ef3044211ed080
thillairavi <thillairavi@gmail.com>
2017-03-10 10:55:05 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 
 var config = {
@@ -16,6 +17,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 
 var articles = { 
@@ -129,6 +131,28 @@ function hash(input, salt) {
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this is random string');
     res.send(hashedString);
+});
+
+
+app.post('/user', function (req, res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    var salt = crypto.getRandomBytes('128').tostring('hex');
+    var dbString = hash(password, salt);
+    
+     pool.query('INSERT INTO  "user1" (username, password) VALUES ($1,$2)', [username,dbString] , function(err,result){
+        if(err){
+         res.status(500).send(err,toString());
+        }
+        else{
+            res.send('User sucessfully created:' +username);
+            
+        }
+    });
+    
+    
 });
 
 
