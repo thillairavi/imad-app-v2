36bfa11185db874f71a1dbc312302ef31069e965
thillairavi <thillairavi@gmail.com>
2017-03-11 08:59:21 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,6 +46,45 @@ button.onclick = function() {
 
 
 //print the name when press submit
+var submit= document.getElementById("submit_btn");
+
+submit.onclick = function(){
+    //Make a request 
+    var request= new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take action
+            if(request.status === 200){
+                alert("Logged in successfully");
+            }
+            else if(request.status === 403){
+                alert("username/password incorrect");
+            }
+            else if(request.status === 500){
+                alert("something went wrong in the server");
+            }    
+                
+            
+          
+        }
+          //else not yet
+    };
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    console.log(username);
+    console.log(password);
+    // make a request
+    request.open("POST", "https://thillairavi.imad.hasura-app.io/login", true);
+    request.setRequestHeader('Content-Type' , '/application/json');
+    request.send(JSON.stringify({username: username, password:password}));
+};
+
+
+
+/*
+//print the name when press submit
 var submit= document.getElementById("submit-btn");
 
 submit.onclick = function(){
@@ -76,4 +115,4 @@ submit.onclick = function(){
     request.open("GET", "https://thillairavi.imad.hasura-app.io/submit-name?name=" + name, true);
     request.send(null);
 };
-
+*/
