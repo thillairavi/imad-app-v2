a3cb2ef3440f5f76bbe94eed1db81563997ef483
thillairavi <thillairavi@gmail.com>
2017-03-13 09:20:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 
 var config = {
   user: 'thillairavi',
@@ -18,6 +18,11 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
+    
+}));
 
 
 var articles = { 
